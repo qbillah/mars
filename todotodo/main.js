44fa7f78c47e78483ba1f0d24796553efc0b3dc8
Quazi Billah <info@antiantianti.com>
2019-05-04 12:04:18 -0400

    front-end almost done

--- todotodo/main.js
+++ todotodo/main.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
 
-    $('.todo-item-wrap').draggable({ revert: true });
-
-    $("#add").click(function(){
-
+    function addNewElement(a , b , c){
         var todoItem = document.createElement("div");
         todoItem.className = "todo-item-wrap";
         $(todoItem).css("opacity" , "0");
@@ -14,28 +11,34 @@ $(document).ready(function(){
         var actions = document.createElement("div");
         actions.className = "actions";
 
+        /**CHANGE THIS WITH THE EMOJI ALGORITHM */
+        /**CHANGE ELEMENT TO A SPAN - NO CHECKBOXES */
         var checked = document.createElement("input");
         $(checked).attr("type" , "checkbox");
         checked.className = "done";
+         /**CHANGE THIS WITH THE EMOJI ALGORITHM */
 
         var content = document.createElement("div");
         content.className = "content";
 
         var title = document.createElement("input");
         title.className = "rem-content";
+        $(title).css("color" , "black");
         $(title).attr("type" , "text");
-        $(title).attr("placeholder" , "Untitled");
+        $(title).val(a);
 
         var br = document.createElement("br");
 
         var desc = document.createElement("input");
         desc.className = "rem-content";
+        $(desc).css("color" , "black");
         $(desc).attr("type" , "text");
-        $(desc).attr("placeholder" , "Untitled");
+        $(desc).val(b);
 
         var date = document.createElement("input");
         date.className = "rem-content";
         $(date).attr("type" , "date");
+        $(date).val(c);
 
         var edit = document.createElement("div");
         edit.className = "edit";
@@ -56,9 +59,40 @@ $(document).ready(function(){
         }, 5);
         $(todoItem).draggable({ revert: true })
         document.getElementById("app").appendChild(todoItem);
+    }
+
+    $('.todo-item-wrap').draggable({ revert: true });
+
+    $("#add").click(function(){
+        if($('#title').val() != '' && $('#date').val() != ''){
+            var title = $('#title').val();
+            var date = $('#date').val();
+            if($('#description').val() != ''){
+                var desc = $('#description').val();
+            }else{
+                desc = "...";
+            }
+            addNewElement(title , desc , date);
+        }
+        $('#title').val('');
+        $('#description').val('');
+        $('#date').val('');
     });
 
     $('#delete').droppable({
+        //delete element
+        accept: ".todo-item-wrap",
+        drop: function( event, ui ) {
+            ui.draggable.slideUp();
+            console.log(
+                $(ui.draggable).attr('class')
+            );
+        }
+    });
+
+    $('#done').droppable({
+        //element completed
+        accept: ".todo-item-wrap",
         drop: function( event, ui ) {
             ui.draggable.slideUp();
             console.log(
