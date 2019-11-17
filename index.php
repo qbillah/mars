c0a0449ddc319321f11b029aad228473feccdf57
Quazi Billah <info@antiantianti.com>
2019-11-17 18:46:48 -0500

    gtrfe

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
         <script src="jquery.ui.touch-punch.min.js"></script>
-        <script src="main.js"></script>
-        <script src="emoji.js"></script>
+        <script src="../scripts/main.js"></script>
+        <script src="../scripts/emoji.js"></script>
         <style>
             @import url('https://fonts.googleapis.com/css?family=Inconsolata:400,700');
             ::-webkit-scrollbar{
