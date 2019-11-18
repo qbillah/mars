cc775335f09e553249a83277cd25f5a7440edf09
Quazi Billah <info@antiantianti.com>
2019-11-18 13:40:58 -0500

    SORT FINISHED

--- scripts/routing.js
+++ scripts/routing.js
@@ -8,4 +8,24 @@ $(document).ready(function(){
         $(".login-modal").slideDown().css('display' , 'inline-flex');
     });
     
+    $("#IMP").click(function(){
+        window.location.href = "https://mars-remind.herokuapp.com/search?tag=WRK";
+    });
+
+    $("#EML").click(function(){
+        window.location.href = "https://mars-remind.herokuapp.com/search?tag=EML";
+    });
+
+    $("#WRK").click(function(){
+        window.location.href = "https://mars-remind.herokuapp.com/search?tag=WRK";
+    });
+
+    $("#LZY").click(function(){
+        window.location.href = "https://mars-remind.herokuapp.com/search?tag=LZY";
+    });
+
+    $("#misc").click(function(){
+        window.location.href = "https://mars-remind.herokuapp.com/search?tag=";
+    });
+
 });
