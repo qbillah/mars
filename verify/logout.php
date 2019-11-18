4ca133f896e417a994e28347b2ee955686971f99
Quazi Billah <info@antiantianti.com>
2019-11-18 09:09:25 -0500

    iojuh

--- verify/logout.php
+++ verify/logout.php
@@ -2,8 +2,10 @@
     session_start();
     unset($_SESSION["userID"]);
     unset($_SESSION["uuID"]);
+    unset($_SESSION["email"]);
     setcookie('userID', "", time()-7200 , '/');
     setcookie('uuID', "", time()-7200 , '/');
+    setcookie('email', "", time()-7200 , '/');
     session_destroy();
     header("Location: https://mars-remind.herokuapp.com");
 ?>
