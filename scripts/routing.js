c505f62cfd77e418637170ab6251ca8386ad5b1d
Quazi Billah <info@antiantianti.com>
2019-11-17 20:35:43 -0500

    iuhyhgvjj

--- scripts/routing.js
+++ scripts/routing.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
         var eventDescription = $("#description").val();
         var eventDate = document.getElementById("date").value;
 
-        $.post( "https://mars-remind.herokuapp.com/verify/addEvent", { title: eventTitle })
+        $.post( "https://mars-remind.herokuapp.com/verify/addEvent", { title: eventTitle , description: eventDescription , date: eventDate})
             .done(function( data ) {
                 alert( "Data Loaded: " + data );
         });
