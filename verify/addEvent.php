4a4703622b70341ebe2fea7e7834c86eb0f05480
Quazi Billah <info@antiantianti.com>
2019-11-17 21:06:16 -0500

    ijhuhg

--- verify/addEvent.php
+++ verify/addEvent.php
@@ -11,18 +11,15 @@
             $title = $_POST["title"];
             $title = htmlspecialchars($title);
             $title = filter_var($title , FILTER_SANITIZE_STRING);
-            echo $title;
 
             $date = $_POST["date"];
             $date = htmlspecialchars($date);
             $date = filter_var($date , FILTER_SANITIZE_STRING);
-            echo $date;
 
             if(isset($_POST["description"])){
                 $desc = $_POST["description"];
                 $desc = htmlspecialchars($desc);
                 $desc = filter_var($desc , FILTER_SANITIZE_STRING);
-                echo $desc;
             }
             //SANITIZATION
 
@@ -38,7 +35,17 @@
 
             $sql = "INSERT INTO $table (`event` , `eventDescription` , `eventDate`) VALUES ('$title' , '$desc' , '$date')";
             if($conn->query($sql)){
+
                 echo "Added 1 Event :)";
+
+                if(isset($_SESSION['email'])){
+                    $sendTo = $_SESSION['email'];
+                }else if(isset($_COOKIE['email'])){
+                    $sendTo = $_COOKIE["email"];
+                }else{
+                    header("Location: $redurl");
+                }
+
             }else{
                 echo "Event add error :(";
             }
